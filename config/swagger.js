0dce5fb6d87b7132616aa86d6970737f7f7a3a5d
JyotiBaisoya <jyotibaisoya2508@gmail.com>
2023-09-04 12:24:55 +0530

    done

--- config/swagger.js
+++ config/swagger.js
@@ -2,11 +2,28 @@ const swaggerJsdoc = require('swagger-jsdoc');
 const yaml = require('yamljs');
 
 
+// const options = {
+//   swaggerDefinition: {
+//     openapi: '3.0.0', 
+//     info: {
+//       title: 'API Documentation', 
+//       version: '1.0.0', 
+//       description: 'API documentation for your Express.js application',
+//     },
+//   },
+//   apis: ['./routes/*.js'], 
+// };
+
+
+// const specs = swaggerJsdoc(options);
+
+// module.exports = specs;
+
 const options = {
   swaggerDefinition: {
     openapi: '3.0.0', 
     info: {
-      title: 'API Documentation', 
+      title: 'Your API Documentation', 
       version: '1.0.0', 
       description: 'API documentation for your Express.js application',
     },
